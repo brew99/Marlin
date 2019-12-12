e3ecc63e0d57daa053cc939200e710091a7b46af
brew99 <brew_c99@yahoo.com>
2019-12-12 15:07:37 -0800

    update to allow mass storage

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1624,8 +1624,8 @@
  *
  * Use CRC checks and retries on the SD communication.
  */
-#define SD_CHECK_AND_RETRY 
-// causes SD format popup in windows. comment out to allow mass storage via USB
+//#define SD_CHECK_AND_RETRY 
+// causes SD format popup in windows for BTT boards. comment out to allow mass storage via USB
 
 /**
  * LCD Menu Items
